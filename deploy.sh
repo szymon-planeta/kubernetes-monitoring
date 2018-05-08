abbb28859707d058504bd33d86b826edc8cb2b95
Cpt Planeta <planetaszymon@gmail.com>
2018-05-08 11:52:15 +0200

    kubelets https

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 helm install coreos/prometheus-operator --name prometheus-operator
-helm install coreos/exporter-kubelets --name exporter-kubelets --set https=false
+helm install coreos/exporter-kubelets --name exporter-kubelets
 helm install coreos/prometheus --name prometheus -f prometheus.yml
 helm install coreos/grafana --name grafana --set service.type=NodePort
