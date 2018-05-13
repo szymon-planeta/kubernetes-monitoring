d92f9f056d16a43def279ec4ab9c8002fe213749
Cpt Planeta <planetaszymon@gmail.com>
2018-05-13 12:38:56 +0200

    deploy kube-prometheus

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
-#helm install coreos/prometheus-operator --name prometheus-operator
+helm install coreos/prometheus-operator --name prometheus-operator  --namespace monitoring
+
 #helm install coreos/exporter-kubelets --name exporter-kubelets
 #helm install coreos/prometheus --name prometheus -f prometheus.yml
 #helm install coreos/grafana --name grafana --set service.type=NodePort
+helm install coreos/kube-prometheus --name kube-prometheus --namespace monitoring
 
-kubectl create -f ./metrics-server
+#kubectl create -f ./metrics-server
