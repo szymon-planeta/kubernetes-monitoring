3e28c6f801f7e862303271baafe811d3d2624eeb
Cpt Planeta <planetaszymon@gmail.com>
2018-05-15 10:17:46 +0200

    Deploy kube-prometheus without Grafana

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ echo "PATH=$GOPATH/bin:/usr/lib/go-1.10/bin:$PATH" >> /etc/environment
 go get -u github.com/rakyll/hey
 
 kubectl create -f ./metrics-server
-helm install coreos/prometheus --name grafana-prometheus --namespace monitoring -f prometheus.yml 
-helm install coreos/grafana --name grafana --namespace monitoring --set service.type=NodePort
-helm install coreos/exporter-kubelets --name exporter-kubelets --namespace monitoring
-helm install coreos/exporter-kube-state --name exporter-kube-state --namespace monitoring
+#helm install coreos/prometheus --name grafana-prometheus --namespace monitoring -f prometheus.yml 
+#helm install coreos/grafana --name grafana --namespace monitoring --set service.type=NodePort
+#helm install coreos/exporter-kubelets --name exporter-kubelets --namespace monitoring
+#helm install coreos/exporter-kube-state --name exporter-kube-state --namespace monitoring
+helm install coreos/kube-prometheus --name kube-prometheus --namespace monitoring -f kube-prometheus.yml
 
 git clone https://github.com/stefanprodan/k8s-prom-hpa.git && cd k8s-prom-hpa && kubectl create -f ./podinfo/podinfo-svc.yaml,./podinfo/podinfo-dep.yaml && kubectl create -f ./podinfo/podinfo-hpa.yaml
 
 
-#helm install coreos/kube-prometheus --name kube-prometheus --namespace monitoring -f kube-prometheus.yml
 
