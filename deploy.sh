6daa87f5325e5bcc298ac931499f3b0ac5d854b0
Cpt Planeta <planetaszymon@gmail.com>
2018-05-15 13:01:18 +0200

    Fix paths

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,15 @@
 helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 helm install coreos/prometheus-operator --name prometheus-operator  --namespace monitoring
 
-mkdir -p /root/go
+go="/root/go"
 
+mkdir -p $go
 # Install golang and hey
-GOPATH="/root/go"
-echo "GOPATH=$GOPATH" >> /etc/environment
+export "GOPATH=$go" >> /etc/environment
+echo "$GOPATH" >> /etc/environment
 add-apt-repository ppa:gophers/archive -y && apt-get update -y && apt-get install golang-1.10-go -y
-echo "PATH=$GOPATH/bin:/usr/lib/go-1.10/bin:$PATH" >> /etc/environment
+export "PATH=$GOPATH/bin:/usr/lib/go-1.10/bin:$PATH" 
+echo "$PATH" >> /etc/environment
 go get -u github.com/rakyll/hey
 
 kubectl create -f ./metrics-server
