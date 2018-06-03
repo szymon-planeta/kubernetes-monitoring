29fa287decdc1d946dc1179329441edef2fe4025
Szymon Planeta <planetaszymon@gmail.com>
2018-06-03 21:57:20 +0000

    Delete, scrape

--- delete.sh
+++ delete.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-kubectl delete -f ./podinfo-hpa.yml
+kubectl delete -f ./podinfo
 kubectl delete -f custom-metrics.yml
 kubectl delete -f ./predicted-cpu-usage
 helm delete --purge kube-prometheus
 kubectl delete -f ./metrics-server
-kubectl delete -f ./podinfo
